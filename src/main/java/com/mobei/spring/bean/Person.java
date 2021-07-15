2050a2fa3b6458f3454bc775f6ee899cd7add778
mobei <mobei615@foxmail.com>
2021-07-16 07:09:44 +0800

    Initial commit

---
+++ src/main/java/com/mobei/spring/bean/Person.java
@@ -0,0 +1,15 @@
+package com.mobei.spring.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    private String name;
+
+    private int age;
+}
---
+++ src/main/java/com/mobei/spring/test/MainTest.java
@@ -0,0 +1,16 @@
+package com.mobei.spring.test;
+
+import com.mobei.spring.bean.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        // 读取类路径下的文件
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        Person person = applicationContext.getBean(Person.class);
+        System.out.println(person);
+    }
+
+}
