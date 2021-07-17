b5b9ca2216956fbc3f3ed6ffb95d42fda597081b
mobei <mobei615@foxmail.com>
2021-07-17 15:47:49 +0800

    自动注入相关的几个注解:@Autowired、@Resource、@Inject等等

---
+++ src/main/java/com/mobei/spring/annotation/MainConifgOfAutowired.java
@@ -0,0 +1,58 @@
+package com.mobei.spring.annotation;
+
+import com.mobei.spring.bean.Car;
+import com.mobei.spring.bean.Color;
+import com.mobei.spring.dao.BookDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * 自动装配: spring利用依赖注入（DI），完成对IOC容器中中各个组件的依赖关系赋值
+ *
+ * 1）、@Autowired：自动注入
+ * 		1）、默认优先按照类型去容器中找对应的组件: applicationContext.getBean(BookDao.class),找到就赋值
+ * 		2）、如果找到多个相同类型的组件,再将属性的名称作为组件的id去容器中查找
+ * 		3）、@Qualifier("bookDao"): 使用@Qualifier指定需要装配的组件的id,而不是使用属性名
+ * 		4）、自动装配默认一定要将属性赋值好,没有就会报错: 可以使用@Autowired(required=false);
+ * 		5）、@Primary: 让Spring进行自动装配的时候,默认使用首选的bean,也可以继续使用@Qualifier指定需要装配的bean的名字
+ * 		BookService{
+ * 			@Autowired
+ * 			BookDao  bookDao;
+ * 		}
+ *
+ * 2）、Spring还支持使用@Resource(JSR250)和@Inject(JSR330)[java规范的注解]
+ * 		@Resource:
+ * 			可以和@Autowired一样实现自动装配功能,默认是按照组件名称进行装配的,
+ * 			没有能支持@Primary功能,没有支持@Autowired的reqiured=false功能
+ * 		@Inject:
+ * 			需要导入javax.inject的包,和Autowired的功能一样,没有required=false的功能；
+ *  @Autowired:Spring定义的, @Resource、@Inject都是java规范
+ *
+ */
+@Configuration
+@ComponentScan({"com.mobei.spring.bean"})
+public class MainConifgOfAutowired {
+
+    @Primary
+    @Bean("bookDao2")
+    public BookDao bookDao(){
+        BookDao bookDao = new BookDao();
+        bookDao.setLabel("2");
+        return bookDao;
+    }
+
+    /**
+     * @Bean标注的方法创建对象的时候，方法参数的值从容器中获取
+     * @param car
+     * @return
+     */
+    @Bean
+    public Color color(Car car){
+        Color color = new Color();
+        color.setCar(car);
+        return color;
+    }
+
+}
