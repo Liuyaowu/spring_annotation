83d4ef414368bb600147acceba7d11e80c1bc9e9
mobei <mobei615@foxmail.com>
2021-07-18 15:43:59 +0800

    抽取公共的切入点表达式

--- spring_aop_module/src/main/java/com/mobei/aop/LogAspects.java
+++ spring_aop_module/src/main/java/com/mobei/aop/LogAspects.java
@@ -12,9 +12,19 @@ import org.aspectj.lang.annotation.*;
 public class LogAspects {
 
     /**
-     * 测试前置通知: 目标方法MathCalculator.div执行之前运行
+     * 抽取公共的切入点表达式
      */
-    @Before("execution(public int com.mobei.aop.MathCalculator.*(..))")
+    @Pointcut("execution(int com.mobei.aop.MathCalculator.*(..))")
+    public void pointCut() {
+
+    }
+
+    /**
+     * 测试前置通知: 目标方法MathCalculator.div执行之前运行
+     *
+     * 本类引用可以直接写这个类的方法名即可
+     */
+    @Before("pointCut()")
     public void logStart(JoinPoint joinPoint) {
         System.out.println("业务方法名" + joinPoint.getSignature().getName());
         System.out.println("logStart--- 前置通知: {}");
@@ -22,21 +32,21 @@ public class LogAspects {
 
     /**
      * 测试后置通知: 目标方法MathCalculator.div运行结束之后运行,正常、异常结束都会执行
+     *
+     * 切点表达式如果在外部类可以使用类全路径加方法名的方式
      */
-    @After("execution(public int com.mobei.aop.MathCalculator.*(..))")
-    public void logEnd(JoinPoint joinPoint) {
+    @After("com.mobei.aop.LogAspects.pointCut()")
+    public void logEnd() {
         System.out.println("logEnd--- 后置通知");
     }
 
     /**
      * 测试返回通知: 目标方法MathCalculator.div正常返回之后执行
+     *
      * value: 指定切点表达式
      * returning: 执行目标方法返回值参数名
      */
-    @AfterReturning(
-            value = "execution(public int com.mobei.aop.MathCalculator.*(..))",
-            returning = "returnResult"
-    )
+    @AfterReturning(value = "pointCut()", returning = "returnResult")
     public void logReturn(Object returnResult) {
         System.out.println("logReturn--- 返回通知: 返回值{" + returnResult + "}");
     }
@@ -47,11 +57,9 @@ public class LogAspects {
      * value: 切点表达式
      * exception: 接收异常的方法名
      */
-    @AfterThrowing(value = "execution(public int com.mobei.aop.MathCalculator.*(..))",
-        throwing = "exception"
-    )
+    @AfterThrowing(value = "pointCut()", throwing = "exception")
     public void logException(Exception exception) {
-        System.out.println("logException--- 异常通知: {" + exception+ "}");
+        System.out.println("logException--- 异常通知: {" + exception + "}");
     }
 
 }
