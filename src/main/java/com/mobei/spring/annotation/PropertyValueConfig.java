dbfab006f7b99a27a1eb2d55275fb05625f9356c
mobei <mobei615@foxmail.com>
2021-07-20 06:55:02 +0800

    @ImportResource注解

--- src/main/java/com/mobei/spring/annotation/PropertyValueConfig.java
+++ src/main/java/com/mobei/spring/annotation/PropertyValueConfig.java
@@ -2,14 +2,19 @@ package com.mobei.spring.annotation;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 
 /**
  * 使用@PropertySource加载外部配置文件中的k-v并保存到运行时的环境变量中
+ *
+ * @PropertySource: 导入resource目录下的prooperties文件
+ * @ImportResource: 导入resource目录下的xml文件
  */
 @ComponentScan(value = "com.mobei.spring.bean")
 @PropertySource(value = {"classpath:/person.properties"})
+@ImportResource(value = {"classpath:/xxx.xml"})
 @Configuration
 public class PropertyValueConfig {
 }
