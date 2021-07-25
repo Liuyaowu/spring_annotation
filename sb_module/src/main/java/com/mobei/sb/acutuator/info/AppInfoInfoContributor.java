b3a80e41fca2afe3f819cd080153ea124577ed4f
mobei <mobei615@foxmail.com>
2021-07-25 16:06:58 +0800

    通过代码自定义info信息

---
+++ sb_module/src/main/java/com/mobei/sb/acutuator/info/AppInfoInfoContributor.java
@@ -0,0 +1,22 @@
+package com.mobei.sb.acutuator.info;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+/**
+ * 自定义info信息
+ */
+@Component
+public class AppInfoInfoContributor implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("msg","你好")
+                .withDetail("hello","atguigu")
+                .withDetails(Collections.singletonMap("world","666600"));
+    }
+
+}
