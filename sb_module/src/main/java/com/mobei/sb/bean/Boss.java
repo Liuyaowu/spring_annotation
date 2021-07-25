50acb2f18f3d0ba941fbcda22c3026d4b691117b
mobei <mobei615@foxmail.com>
2021-07-25 17:31:44 +0800

    使用@Profile指定类或者方法使用环境

---
+++ sb_module/src/main/java/com/mobei/sb/bean/Boss.java
@@ -0,0 +1,17 @@
+package com.mobei.sb.bean;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile(value = {"prod", "dev"})
+@Component
+@ConfigurationProperties("person")
+@Data
+public class Boss implements Person {
+
+    private String name;
+    private Integer age;
+
+}
---
+++ sb_module/src/main/java/com/mobei/sb/bean/Person.java
@@ -0,0 +1,8 @@
+package com.mobei.sb.bean;
+
+public interface Person {
+
+   String getName();
+   Integer getAge();
+
+}
